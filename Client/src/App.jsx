7cff64634da820b41f91db92db9aa48d712cdef1
Joe <Joe1972mama1@gmail.com>
2024-09-26 21:04:58 +0300

    Modified a file

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className={"text-red-500"}>Hello</h1>;
+  return (
+      <div>
+        <h1 className={"text-7xl"}>Hello</h1>
+      </div>
+  );
 }
 
 export default App;
