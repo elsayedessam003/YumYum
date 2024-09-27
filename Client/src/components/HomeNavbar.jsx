3843c348b16bf6ea86dc2bc128825eeef9b1c563
elsayedessam003 <elsayedessame@gmail.com>
2024-09-28 00:42:03 +0300

    Responsive Fixed

--- Client/src/components/HomeNavbar.jsx
+++ Client/src/components/HomeNavbar.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import Register from "./Register/Register.jsx";
 import { useState } from "react";
 
 HomeNavbar.propTypes = {
@@ -9,42 +8,61 @@ HomeNavbar.propTypes = {
 
 function HomeNavbar({ setLogin, setSignUp }) {
   const [active, setActive] = useState(false);
+
   return (
-    <div
-      className={
-        "h-[7rem] px-[7%] md:px-[5%] md:h-[8rem] w-screen pt-8 flex items-center justify-between relative overflow-hidden"
-      }
-    >
-      <img
-        src="/public/Logo-White.svg"
-        alt="YamYam logo"
-        className={"h-full"}
-      />
-      <button
-        className={"md:hidden text-white text-4xl"}
-        onClick={() => {
-          setActive(!active);
-        }}
-      >
-        test
-      </button>
-      <div
-        className={`flex items-center gap-10 ${active ? "left-0" : ""} md:flex absolute md:relative left-96 md:left-0 transition-all bg-black w-full `}
-      >
+    <div className="h-[7rem] px-[7%] md:px-[5%] md:h-[8rem] w-screen pt-8 flex items-center justify-between relative">
+      <img src="/public/Logo-White.svg" alt="YamYam logo" className="h-full" />
+
+      <div className="hidden md:flex items-center gap-10">
         <button
-          className={"text-white text-2xl hover:text-project-orange transition"}
-          onClick={() => {
-            setLogin(true);
-          }}
+          className="text-white text-2xl hover:text-project-orange transition"
+          onClick={() => setLogin(true)}
         >
           Login
         </button>
         <button
-          className={
-            "text-white text-2xl py-4 px-6 bg-project-orange rounded-full hover:bg-opacity-90 active:bg-opacity-80"
-          }
+          className="text-white text-2xl py-4 px-6 bg-project-orange rounded-full hover:bg-opacity-90 active:bg-opacity-80 transition"
+          onClick={() => setSignUp(true)}
+        >
+          Register
+        </button>
+      </div>
+
+      <button
+        className="md:hidden text-white text-4xl"
+        onClick={() => setActive(!active)}
+      >
+        {active ? "✕" : "☰"}
+      </button>
+
+      <div
+        className={`flex flex-col items-center gap-8 transition-all ease-in-out duration-300 ${
+          active ? "right-0 opacity-100" : "right-[-100%] opacity-0"
+        } md:hidden fixed top-0 right-0 w-[40%] h-[60%] bg-black
+    bg-opacity-60 backdrop-blur-lg z-20 transition-all p-10 rounded-l-md`}
+      >
+        <button
+          className="self-end text-white text-2xl mb-6"
+          onClick={() => setActive(false)}
+        >
+          ✕
+        </button>
+
+        <button
+          className="text-white text-2xl hover:text-project-orange transition w-full text-center py-4 bg-gray-800 rounded-md"
+          onClick={() => {
+            setLogin(true);
+            setActive(false);
+          }}
+        >
+          Login
+        </button>
+
+        <button
+          className="text-white text-2xl py-4 px-6 bg-project-orange rounded-md hover:bg-opacity-90 active:bg-opacity-80 transition w-full text-center"
           onClick={() => {
             setSignUp(true);
+            setActive(false);
           }}
         >
           Register
--- Client/src/components/Register/Register.jsx
+++ Client/src/components/Register/Register.jsx
@@ -8,112 +8,106 @@ import style from "./Register.module.css";
 import RegisterPasswordInput from "./RegisterPasswordInput.jsx";
 
 Register.propTypes = {
-  login: PropTypes.bool.isRequired,
-  signUp: PropTypes.bool.isRequired,
-  setLogin: PropTypes.func.isRequired,
-  setSignUp: PropTypes.func.isRequired,
+login: PropTypes.bool.isRequired,
+signUp: PropTypes.bool.isRequired,
+setLogin: PropTypes.func.isRequired,
+setSignUp: PropTypes.func.isRequired,
 };
 
 function Register({ login, signUp, setLogin, setSignUp }) {
-  function handleLoginSwitch() {
-    setLogin(true);
-    setSignUp(false);
-  }
+function handleLoginSwitch() {
+setLogin(true);
+setSignUp(false);
+}
 
-  function handleSignUpSwitch() {
-    setLogin(false);
-    setSignUp(true);
-  }
+function handleSignUpSwitch() {
+setLogin(false);
+setSignUp(true);
+}
 
-  function handleClose(e) {
-    if (e.target === e.currentTarget) {
-      setLogin(false);
-      setSignUp(false);
-    }
-  }
+function handleClose(e) {
+if (e.target === e.currentTarget) {
+setLogin(false);
+setSignUp(false);
+}
+}
 
-  return (
-    <div
-      className={
-        "z-10 w-screen h-screen absolute bg-black bg-opacity-40 flex justify-center items-center overflow-hidden"
-      }
-      onClick={handleClose}
-    >
-      <div
-        className={`w-[32rem] bg-white rounded-xl flex flex-col gap-10 p-10 ${style.opened} select-none transition-all`}
-      >
-        <div className={"flex"}>
-          <RegisterSelectorButton isActive={login} handler={handleLoginSwitch}>
-            Login
-          </RegisterSelectorButton>
+return (
+<div
+  className={ "z-10 w-screen h-screen absolute bg-black bg-opacity-40 flex justify-center items-center overflow-hidden"
+  } onClick={handleClose}>
+  <div className={`w-[32rem] bg-white rounded-xl flex flex-col gap-10 p-10 ${style.opened} select-none transition-all`}>
+    <div className={"flex"}>
+      <RegisterSelectorButton isActive={login} handler={handleLoginSwitch}>
+        Login
+      </RegisterSelectorButton>
 
-          <RegisterSelectorButton
-            isActive={signUp}
-            handler={handleSignUpSwitch}
-          >
-            Register
-          </RegisterSelectorButton>
-        </div>
-
-        {login && <LoginInputs />}
-        {signUp && <SignUpInputs />}
-      </div>
+      <RegisterSelectorButton isActive={signUp} handler={handleSignUpSwitch}>
+        Register
+      </RegisterSelectorButton>
     </div>
-  );
+
+    {login &&
+    <LoginInputs />}
+    {signUp &&
+    <SignUpInputs />}
+  </div>
+</div>
+);
 }
 
 function LoginInputs() {
-  return (
-    <>
-      <div className={"flex flex-col gap-4"}>
-        <RegisterInput type={"text"} placeHolder={"Email"}>
-          <MdEmail />
-        </RegisterInput>
+return (
+<>
+  <div className={"flex flex-col gap-4"}>
+    <RegisterInput type={"text"} placeHolder={"Email"}>
+      <MdEmail />
+    </RegisterInput>
 
-        <RegisterPasswordInput />
-      </div>
+    <RegisterPasswordInput/>
+  </div>
 
-      <div className={"w-full"}>
-        <RegisterButton>Login</RegisterButton>
-      </div>
-    </>
-  );
+  <div className={"w-full"}>
+    <RegisterButton>Login</RegisterButton>
+  </div>
+</>
+);
 }
 
 function SignUpInputs() {
-  return (
-    <>
-      <div className={"flex flex-col gap-4"}>
-        <RegisterInput type={"text"} placeHolder={"Full Name"}>
-          <BsFillPersonFill />
-        </RegisterInput>
+return (
+<>
+  <div className={"flex flex-col gap-4"}>
+    <RegisterInput type={"text"} placeHolder={"Full Name"}>
+      <BsFillPersonFill/>
+    </RegisterInput>
 
-        <RegisterInput type={"email"} placeHolder={"Email"}>
-          <MdEmail />
-        </RegisterInput>
+    <RegisterInput type={"email"} placeHolder={"Email"}>
+      <MdEmail/>
+    </RegisterInput>
 
-        <RegisterPasswordInput />
+    <RegisterPasswordInput/>
 
-        <RegisterPasswordInput placeHolder={"Re-Password"} />
-      </div>
+    <RegisterPasswordInput placeHolder={"Re-Password"} />
+  </div>
 
-      <div className={""}>
-        By tapping “Register” you agree to Yam-Yam’s{" "}
-        <a href="#" className={"text-project-orange underline"}>
-          Terms and Conditions
-        </a>{" "}
-        and{" "}
-        <a href="#" className={"text-project-orange underline"}>
-          Privacy policy
-        </a>
-        .
-      </div>
+  <div className={""}>
+    By tapping “Register” you agree to Yam-Yam’s{" "}
+    <a href="#" className={"text-project-orange underline"}>
+      Terms and Conditions
+    </a>{" "}
+    and{" "}
+    <a href="#" className={"text-project-orange underline"}>
+      Privacy policy
+    </a>
+    .
+  </div>
 
-      <div className={"w-full flex flex-col gap-4"}>
-        <RegisterButton>Register</RegisterButton>
-      </div>
-    </>
-  );
+  <div className={"w-full flex flex-col gap-4"}>
+    <RegisterButton>Register</RegisterButton>
+  </div>
+</>
+);
 }
 
 export default Register;
