e6763bda75f8614cc744baf4801085008c286050
elsayedessam003 <elsayedessame@gmail.com>
2024-10-01 01:41:02 +0300

    restaurants search bar in homepage fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
-import "./global.css";
-import UserProvider from "./context/UserProvider.jsx";
 import Restaurants from "./pages/restaurants/Restaurants.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import UserProvider from "./context/UserProvider.jsx";
+import PropTypes from 'prop-types';
+import "./global.css";
+
+RestaurantLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+function RestaurantLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <UserProvider>
         <Routes>
-          <Route path={"/"} element={<Home />}></Route>
-        </Routes>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path={"/restaurants"} element={<Restaurants />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/restaurants"
+            element={
+              <RestaurantLayout>
+                <Restaurants />
+              </RestaurantLayout>
+            }
+          />
         </Routes>
       </UserProvider>
     </BrowserRouter>
