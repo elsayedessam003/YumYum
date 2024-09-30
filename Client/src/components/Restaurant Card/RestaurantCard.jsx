21b6e4f01f8909e3ec6b9ec441ef1a0a99010908
Joe <Joe1972mama1@gmail.com>
2024-09-30 23:15:10 +0300

    modified the restaurant card and added a background image to the slider

--- Client/src/components/Restaurant Card/RestaurantCard.jsx
+++ Client/src/components/Restaurant Card/RestaurantCard.jsx
@@ -1,39 +1,58 @@
 import { MdDeliveryDining } from "react-icons/md";
+import PropTypes from "prop-types";
 
-const RestaurantCard = ({ name, rating, reviews, time, fee, imgSrc, profileImgSrc }) => {
-    return (
-        <div className="rounded-lg flex w-[339px] flex-col items-start gap-[23px] hover:shadow-lg">
-            <img
-                src={imgSrc}
-                alt={name}
-                className="h-[225px] self-stretch rounded-[10px] bg-lightgray bg-center bg-cover bg-no-repeat bg-[#F2F2F2]"
-            />
-
-            <div className="flex items-center gap-[9px] self-stretch">
-                <img
-                    src={profileImgSrc}
-                    alt={`${name} profile`}
-                    className="w-[62px] h-[62px] rounded-full bg-cover bg-no-repeat"
-                />
-
-                <div className="flex w-[268px] flex-col items-start gap-1">
-                    <div className="flex justify-between w-full">
-                        <h3 className="text-base font-semibold">{name}</h3>
-                        <div className="flex items-center text-sm text-gray-500">
-                            <span className="mr-2">★ {rating}</span>
-                            <span>({reviews})</span>
-                        </div>
-                    </div>
-
-                    <div className="flex items-center text-sm text-gray-500 mt-1">
-                        <MdDeliveryDining className="mr-2" />
-                        <span className="mr-2">{time} min</span>
-                        <span>• {fee} Fee</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+RestaurantCard.propTypes = {
+  name: PropTypes.string,
+  rating: PropTypes.number,
+  reviews: PropTypes.number,
+  time: PropTypes.number,
+  fee: PropTypes.number,
+  imgSrc: PropTypes.string,
+  profileImgSrc: PropTypes.string,
 };
+
+function RestaurantCard({
+  name,
+  rating,
+  reviews,
+  time,
+  fee,
+  imgSrc,
+  profileImgSrc,
+}) {
+  return (
+    <div className="rounded-lg flex w-[339px] flex-col items-start gap-[23px] hover:shadow-lg hover:scale-105 transition-all ease-linear p-4 cursor-pointer">
+      <img
+        src={imgSrc}
+        alt={name}
+        className="h-[225px] self-stretch rounded-[10px] bg-lightgray bg-center bg-cover bg-no-repeat bg-[#F2F2F2]"
+      />
+
+      <div className="flex items-center gap-[9px] self-stretch">
+        <img
+          src={profileImgSrc}
+          alt={`${name} profile`}
+          className="w-[62px] h-[62px] rounded-full bg-cover bg-no-repeat"
+        />
+
+        <div className="flex w-[268px] flex-col items-start gap-1">
+          <div className="flex justify-between w-full">
+            <h3 className="text-lg font-semibold">{name}</h3>
+            <div className="flex items-center text-lg">
+              <span className="mr-2">★ {rating}</span>
+              <span>(+{reviews})</span>
+            </div>
+          </div>
+
+          <div className="flex items-center text-gray-500 mt-1">
+            <MdDeliveryDining className="mr-2" />
+            <span className="mr-2">{time} min</span>
+            <span>• {fee}£ Fee</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default RestaurantCard;
--- Client/src/pages/restaurants/Restaurants.jsx
+++ Client/src/pages/restaurants/Restaurants.jsx
@@ -1,19 +1,141 @@
 import RestaurantsFilterSection from "../../components/RestaurantsFilterSection/RestaurantsFilterSection.jsx";
+import RestaurantCard from "../../components/Restaurant Card/RestaurantCard.jsx";
+import { BsDot } from "react-icons/bs";
+import backGround from "/CategoriesBackground.svg";
 
 function Restaurants() {
   return (
-    <div className={"flex h-screen"}>
-      <div className={"flex-grow-[1] h-full"}>
+    <div className={"flex"}>
+      <div className={"w-1/4 min-w-[20rem] h-full"}>
         <RestaurantsFilterSection />
       </div>
 
-      <div className={"flex-grow-[9]"}>
-        <div
-          style={{ backgroundImage: "url('/CategoriesBackground.svg')" }}
-          className={"bg-cover bg-center w-full h-auto"}
-        ></div>
+      <div className={"w-full overflow-hidden"}>
+        <div>
+          <img src={backGround} alt="" />
+        </div>
 
-        <div></div>
+        <div className={"flex flex-wrap justify-center items-center gap-4"}>
+          <div
+            className={
+              "flex items-center w-full text-4xl font-semibold text-center p-4"
+            }
+          >
+            Restaurants{" "}
+            <section className={"flex items-center text-base h-full"}>
+              <BsDot /> <p>12 results</p>
+            </section>
+          </div>
+
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+          <RestaurantCard
+            name={"Potato"}
+            fee={9.99}
+            imgSrc={"/public/Frame 48.png"}
+            profileImgSrc={"/public/Ellipse 5.png"}
+            rating={4.2}
+            reviews={200}
+            time={22}
+          />
+        </div>
       </div>
     </div>
   );
