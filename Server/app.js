f94f89552ebb5e7e38039011a55548a31077bcad
Mohamed Amr <momo.amr.f@gmail.com>
2024-10-13 21:50:44 +0300

    connected to atlas instead of localhost

--- Server/app.js
+++ Server/app.js
@@ -1,32 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cloudinary = require('cloudinary').v2;
-const bodyParser = require('body-parser')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
+const bodyParser = require("body-parser");
 
-const AppError = require('./utils/appError');
+const AppError = require("./utils/appError");
 const userRouter = require("./routes/user.route");
 const restaurantRouter = require("./routes/restaurant.route");
-const gRestaurantRouter = require('./routes/global.restaurant.route')
-const errorController = require('./controllers/error.controller')
+const gRestaurantRouter = require("./routes/global.restaurant.route");
+const errorController = require("./controllers/error.controller");
 
-mongoose.connect('mongodb://localhost:27017/yum-yum_DB').then(()=>{
-  console.log('DB connection established');
-})
+// mongoose.connect("mongodb://localhost:27017/yum-yum_DB").then(() => {
+//   console.log("DB connection established");
+// });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  });
 
 cloudinary.config({
   cloud_name: process.env.APP_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.APP_CLOUDINARY_API_KEY ,
-  api_secret: process.env.APP_CLOUDINARY_SECRET_KEY
-})
+  api_key: process.env.APP_CLOUDINARY_API_KEY,
+  api_secret: process.env.APP_CLOUDINARY_SECRET_KEY,
+});
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -38,6 +53,6 @@ app.all("*", (req, res, next) => [
   next(AppError.create("Page not found", ERROR, 404)),
 ]);
 app.use(errorController);
-app.listen(3000,()=>{
-  console.log('server running... ');
-})
+app.listen(3000, () => {
+  console.log("server running... ");
+});
