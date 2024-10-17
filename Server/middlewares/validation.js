bf0680588577a5698ac3b6550aa53b883d61e4f1
Mohamed-Elsayed-1 <mohamedelsayed55086@gmail.com>
2024-10-17 22:28:33 +0300

    "Removed street and city validation from validateUser middleware, reformatted validation rules for validateRestaurant middleware"

--- Server/middlewares/validation.js
+++ Server/middlewares/validation.js
@@ -10,8 +10,6 @@ const handleValidationError = async (req, res, next) => {
 
 exports.validateUser = [
   body("name").isString().notEmpty().withMessage("Name must be a string"),
-  body("street").isString().notEmpty().withMessage("street must be a string"),
-  body("city").isString().notEmpty().withMessage("City must be a string"),
   handleValidationError,
 ];
 
@@ -19,11 +17,23 @@ exports.validateRestaurant = [
   body("restaurantName").notEmpty().withMessage("Restaurant Name is required"),
   body("country").isString().notEmpty().withMessage("Country is required"),
   body("city").notEmpty().withMessage("City is required"),
-  body("deliveryPrice").isFloat({min:0}).withMessage('Delivery Price must be positive number'),
-  body("estimatedDeliveryTime").isFloat({min:0}).withMessage('Estimated delivery time must be positive integar'),
-  body("cuisines").isArray().withMessage('Cuisines must be array').notEmpty().withMessage('Cuisines array cannot be empty'),
-  body("menuItems").isArray().withMessage('Menu Items must be array'),
-  body("menuItems.*.name").notEmpty().withMessage('Menu Items name is required'),
-  body("menuItems.*.price").isFloat({min: 0}).withMessage('Menu Items price is required and must be positive number'),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery Price must be positive number"),
+  body("estimatedDeliveryTime")
+    .isFloat({ min: 0 })
+    .withMessage("Estimated delivery time must be positive integar"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be array")
+    .notEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu Items must be array"),
+  body("menuItems.*.name")
+    .notEmpty()
+    .withMessage("Menu Items name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu Items price is required and must be positive number"),
   handleValidationError,
 ];
